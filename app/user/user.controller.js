105d85c6b3f46476c8dae76f43f9c4f1f63c7835
Nikzin <nikolay.softronic@gmail.com>
2022-06-06 21:47:26 +0200

    Removed properties repeat from users. Placed images in folder. C 2022.

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -41,83 +41,25 @@ angular.module("user").controller("userController", ["$scope", "$location", "$ht
         };
 
         init();
-//do it as init for this page to get the orders and make it as scope , not var.
-/*    userFactoryService.getUserOrders(appServiceFactory.getUserId()).then(function (response) {
 
-
-        userOrders = response.data;
-        var orderProducts = [];
-        var allProducts = [];
-
-        angular.forEach(userOrders, function (order) {
-            orderProducts.push(order.products);
-                   });
-
-/!*        productServiceFactory.getProductsByCategoryId().then(function (response) {
-            allProducts = response.data;
-            var total = 0;
-            angular.forEach(orderProducts , function (products) {
-                angular.forEach(products, function (product) {
-                    angular.forEach(allProducts, function (item) {
-                        if(product.productId == item.id)  {
-                            debugger;
-                            total += (item.price * product.quantity);
-                        }
-                    })
-                });
-            });
-            $scope.totalAmount1 = total;
-        });*!/
-
-
-        productServiceFactory.getProductsByCategoryId().then(function (response) {
-            allProducts = response.data;
-            var total = 0;
-            angular.forEach(orderProducts, function (op) {
-                angular.forEach(allProducts, function (p) {
-                    if (op[0].productId == p.id) {
-                        total += (p.price * op[0].quantity);
-                    }
-                });
-            });
-            $scope.totalAmount1 = total;
-        });
-
-        $scope.userOrders = userOrders;
-    });*/
 
     userFactoryService.getUserInfo(loginServiceFactory.getCustomerId()).then(function (response) {
-        user = response.data;
+        $scope.userForm = response.data;
 
-        $scope.firstname = user.firstName;
-        $scope.lastname = user.lastName;
-        $scope.email = user.email;
-        $scope.phone = user.phone;
-        $scope.address = user.address;
-        $scope.postalcode = user.postalCode;
-        $scope.city = user.city;
-        $scope.password =user.password; //add this
     });
 
     $scope.updateUser = function () {
-        var userInfo = {
-            firstName   :   $scope.firstname,
-            lastName    :   $scope.lastname,
-            email       :   $scope.email,
 
-            phone       :   $scope.phone,
-            address     :   $scope.address,
-            postalCode  :   $scope.postalcode,
-            city        :   $scope.city,
-            password    :   $scope.password
-        };
 
-        userFactoryService.updateUserInfo(user.customerId, userInfo).then(function (response) {
-            console.log(response);
+        let userId = loginServiceFactory.getCustomerId(); //todo null check
+        //userInfo.id=userId;
+        $scope.userForm.id = userId;
 
-            userInfo.id = loginServiceFactory.getCustomerId();
-            delete userInfo['password'];
-            loginServiceFactory.setCustomer(userInfo);
+
+        userFactoryService.updateUserInfo(userId,  $scope.userForm).then(function (response) {
+
+            delete $scope.userForm['password'];
+            loginServiceFactory.setCustomer($scope.userForm);
                });
     };
 
