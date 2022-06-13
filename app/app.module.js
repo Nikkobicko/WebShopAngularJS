6a87d959cb1755e274749d96d469473669fcd863
Nikzin <nikolay.softronic@gmail.com>
2022-06-13 18:08:51 +0200

    Angular current 1.1

--- app/app.module.js
+++ app/app.module.js
@@ -1,7 +1,7 @@
 angular.module("app", ["ui.bootstrap", "ngRoute", "category", "product",
     "cart", "login", "productdetails", "user", "orderdetails", "newUser"])
     .constant('myUrl',{
-        'key1': "http://167.99.222.177:8080/webshop9-0.0.1-SNAPSHOT",
-       /* 'key1': "http://localhost:8080/webshop9",*/
+       /* 'key1': "http://167.99.222.177:8080/webshop9-0.0.1-SNAPSHOT",*/
+       'key1': "http://localhost:8080/webshop9",
 
            } );
